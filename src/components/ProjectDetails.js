3a7ae91ee2cb584c26ec8add67bb9d1dda24b70b
Seregy77 <seregy77@gmail.com>
2018-05-07 17:09:27 +0300

    Split Project into smaller components, polish project page

--- src/components/ProjectDetails.js
+++ src/components/ProjectDetails.js
@@ -22,7 +22,8 @@ class Project extends Component {
     super(props);
     this.state = {
       error: null,
-      isLoaded: false,
+      isProjectLoaded: false,
+      isLocationLoaded: false,
       project: {}
     };
   }
@@ -31,34 +32,42 @@ class Project extends Component {
     axios.get("http://127.0.0.1:8080/v0/projects/" + this.props.id, { crossdomain: true }).then(
       result => {
         this.setState({
-          isLoaded: true,
-          project: result.data
+          isProjectLoaded: true,
+          project: result.data,
         });
       },
       error => {
         this.setState({
-          isLoaded: true,
+          isProjectLoaded: true,
           error
         });
       }
     ).then(() => Geocode.fromLatLng(this.state.project.location.latitude, this.state.project.location.longitude))
     .then(
       response => {
-        const postalCode = ' ' + response.results[0].address_components.filter(component => component.types.includes('postal_code'))[0].long_name;
-        const addressWithoutPostalCode = response.results[0].formatted_address.replace(postalCode, '');
+        console.log(response);
+        const postalCode = response.results[0].address_components.filter(component => component.types.includes('postal_code'));
+        let addressWithoutPostalCode = response.results[0].formatted_address;
+        if (postalCode.length > 0) {
+          addressWithoutPostalCode = addressWithoutPostalCode.replace(' ' + postalCode[0].long_name, '');
+        }
+
+        const location = this.state.project.location;
+        location.formatted_address = addressWithoutPostalCode;
 
         this.setState({
-          projectAddress: addressWithoutPostalCode,
+          isLocationLoaded: true,
+          projectLocation: location,
         });
       },
       error => {
-        projectAddress: error
+        isLocationLoaded: true,
+        error
       })
   }
 
   render() {
-    const { project } = this.state;
-    const { error, isLoaded, projects } = this.state;
+    const { project, projectLocation,  error, isProjectLoaded } = this.state;
 
     const videos = project.gallery_videos || []
     const images = project.gallery_images || []
@@ -67,21 +76,11 @@ class Project extends Component {
       images.push(project.project_image);
     }
 
-    const raised = project.raised || {amount: 0};
-
-    const categories = project.categories || [];
-
-    const eventDate = project.event_date || moment('2018-07-26T02:17:47').format('dddd, MMMM Do, hh:mm');
-
-    const fundingGoal = (project.funding_goal ? project.funding_goal.amount : 1);
-    const progress = Math.floor(raised.amount / fundingGoal * 100);
-    const timeLeft = moment
-                      .duration(moment(project.ends).diff(new moment()))
-                      .format();
+    const eventDate = moment(project.event_date);
 
     if (error) {
       return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
+    } else if (!isProjectLoaded) {
       return <div>Loading...</div>;
     } else {
       return (
@@ -90,44 +89,10 @@ class Project extends Component {
           <div className="row">
             <div className="col-8">
               <ProjectGallery images={images} videos={videos}/>
-              <div className="d-flex">
-                <div className="">
-                  <Link to={"/date/" + eventDate} className="event-date mr-3"><FontAwesomeIcon className="date-icon" icon={faCalendarAlt}/> {eventDate}</Link>
-                </div>
-                <div className="ml-auto">
-                <Link to={"/place/" + this.state.projectAddress} className="location mr-3"><FontAwesomeIcon className="location-icon" icon={faMapMarkerAlt}/> {this.state.projectAddress}</Link>
-                </div>
-              </div>
+              <ProjectFooter eventDate={eventDate} projectAddress={projectLocation}/>
             </div>
             <div className="col-4">
-              <div className="project-info p-2 d-flex flex-column text-left">
-                <div className="progress mt-auto">
-                    <div className="progress-bar" style={{width: progress + '%'}} role="progressbar" aria-valuenow={project.raised.amount} aria-valuemin="0"
-                      aria-valuemax={fundingGoal}></div>
-                </div>
-                <div className="info-block pb-3">
-                  <span className="primary text-primary">{project.raised.amount + "" + getSymbolFromCurrency(project.raised.currency)} </span>
-                  <span className="">{project.raised.currency} raised</span>
-                  <div className="text-muted">{progress}% of {fundingGoal + "" + getSymbolFromCurrency(project.funding_goal.currency)}</div>
-                </div>
-                <div className="info-block pb-3">
-                  <span className="primary">{project.contributions.length}</span>
-                  <div className="text-muted">contributors</div>
-                </div>
-                <div className="info-block pb-3">
-                  <span className="primary">{timeLeft}</span>
-                  <div className="text-muted">left</div>
-                </div>
-                <div className="info-block contribute">
-                  <Link to="/contribute" className="btn btn-primary w-100">Contribute to the project</Link>
-                </div>
-
-                <div className="info-block d-flex">
-                  {categories.map((category) => (
-                    <Link to={"/category/" + category.id} className="category mr-3" key={category.id}><FontAwesomeIcon className="category-icon" icon={faTag}/> {category.name}</Link>
-                  ))}
-                </div>
-              </div>
+              <ProjectSideInfo project={project}/>
             </div>
           </div>
         </div>
@@ -143,6 +108,93 @@ function ProjectHeader(props) {
     <div className="header row mb-4 d-flex flex-column">
       <h2 className="p-1 flex-fill">{project.name}</h2>
       <div className="text-muted p-1 flex-fill">{project.short_description}</div>
+    </div>
+  );
+}
+
+function ProjectFooter(props) {
+  return (
+    <div className="d-flex">
+      <ProjectDate date={props.eventDate}/>
+      {props.projectAddress ? (<ProjectAddress address={props.projectAddress}/>)  : null}
+    </div>
+  );
+}
+
+function ProjectDate(props) {
+  const formattedDate = props.date.format('dddd, MMMM Do, hh:mm');
+  const isoDate = props.date.toISOString();
+
+  return (
+    <div className="mr-auto">
+      <Link to={"/date/" + isoDate} className="event-date"><FontAwesomeIcon className="date-icon" icon={faCalendarAlt}/> {formattedDate}</Link>
+    </div>);
+}
+
+function ProjectAddress(props) {
+  const formattedAddress = props.address.formatted_address;
+  const latitude = props.address.latitude;
+  const longitude = props.address.longitude;
+
+  return (
+    <div className="ml-auto">
+      <Link to={"/place/" + latitude + "," + longitude} className="location"><FontAwesomeIcon className="location-icon" icon={faMapMarkerAlt}/> {formattedAddress}</Link>
+    </div>
+  );
+}
+
+function ProjectSideInfo(props) {
+  const project = props.project;
+
+  const raised = project.raised || {amount: 0};
+
+  const categories = project.categories || [];
+
+  const fundingGoal = project.funding_goal || {amount: 1};
+  const progress = Math.floor(raised.amount / fundingGoal.amount * 100);
+  const timeLeft = moment
+                    .duration(moment(project.ends).diff(new moment()))
+                    .format();
+
+  const owner = project.owner;
+
+  return (
+    <div className="project-info px-2 d-flex flex-column flex-fill text-left">
+      <div className="project-owner info-block">
+        <div className="d-flex">
+          <Link to={'person/' + owner.id}><img className="image" src={project.owner.image_link}/></Link>
+          <div className="d-flex flex-column pl-3">
+            <Link to={'person/' + owner.id}>{owner.name} {owner.surname ? owner.surname : ''}</Link>
+            <div className="text-muted">Project owner</div>
+          </div>
+        </div>
+      </div>
+      <div className="info-block progress">
+        <div className="progress-bar" style={{width: progress + '%'}} role="progressbar" aria-valuenow={raised.amount} aria-valuemin="0" 
+          aria-valuemax={fundingGoal.amount}>
+        </div>
+      </div>
+      <div className="info-block">
+        <span className="primary text-primary">{raised.amount + "" + getSymbolFromCurrency(raised.currency)} </span>
+        <span className="">{raised.currency} raised</span>
+        <div className="text-muted">{progress}% of {fundingGoal.amount + "" + getSymbolFromCurrency(fundingGoal.currency)}</div>
+      </div>
+      <div className="info-block">
+        <span className="primary">{project.contributions.length}</span>
+        <div className="text-muted">contributors</div>
+      </div>
+      <div className="info-block">
+        <span className="primary">{timeLeft}</span>
+        <div className="text-muted">left</div>
+      </div>
+      <div className="info-block contribute">
+        <Link to={"/contribute/" + project.id} className="btn btn-primary w-100">Contribute to the project</Link>
+      </div>
+      <div className="info-block d-flex">
+        {categories.map((category) => (
+          <Link to={"/category/" + category.id} className="category mr-3" key={category.id}><FontAwesomeIcon className="category-icon" icon={faTag}/> {category.name}</Link>
+        ))}
+      </div>
     </div>
   );
 }
