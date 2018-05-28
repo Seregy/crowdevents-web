8f857075079eb2a1daf4952b2191b43fea4e6e5a
Seregy <seregy77@gmail.com>
2018-05-28 18:03:13 +0300

    Move ProjectDetails into Project and ProjectDescription into ProjectDetails

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,12 +1,269 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import moment from "moment";
+import getSymbolFromCurrency from "currency-symbol-map";
+import Geocode from "react-geocode";
+
 import ProjectDetails from "./ProjectDetails";
+import ProjectGallery from "./ProjectGallery";
+
+import FontAwesomeIcon from "@fortawesome/react-fontawesome";
+import faHeart from "@fortawesome/fontawesome-free-regular/faHeart";
+import faLink from "@fortawesome/fontawesome-free-solid/faLink";
+import faEnvelope from "@fortawesome/fontawesome-free-solid/faEnvelope";
+import faTag from "@fortawesome/fontawesome-free-solid/faTag";
+import faMapMarkerAlt from "@fortawesome/fontawesome-free-solid/faMapMarkerAlt";
+import faCalendarAlt from "@fortawesome/fontawesome-free-regular/faCalendarAlt";
+
+import "../css/Project.css";
+
+import avatarPlaceholder from "../image/avatar_placeholder.png";
 
 class Project extends Component {
-  render() {
-    return (
-      <ProjectDetails id={this.props.match.params.id} currentTab={this.props.match.params.tab} />
-    );
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isProjectLoaded: false,
+      isLocationLoaded: false,
+      project: {},
+    };
   }
+
+  componentDidMount() {
+    axios
+      .get("http://127.0.0.1:8080/v0/projects/" + this.props.match.params.id, {
+        crossdomain: true
+      })
+      .then(
+        result => {
+          this.setState({
+            isProjectLoaded: true,
+            project: result.data
+          });
+        },
+        error => {
+          this.setState({
+            isProjectLoaded: true,
+            error
+          });
+        }
+      )
+      .then(() =>
+        Geocode.fromLatLng(
+          this.state.project.location.latitude,
+          this.state.project.location.longitude
+        )
+      )
+      .then(
+        response => {
+          console.log(response);
+          const postalCode = response.results[0].address_components.filter(
+            component => component.types.includes("postal_code")
+          );
+          let addressWithoutPostalCode = response.results[0].formatted_address;
+          if (postalCode.length > 0) {
+            addressWithoutPostalCode = addressWithoutPostalCode.replace(
+              " " + postalCode[0].long_name,
+              ""
+            );
+          }
+
+          const location = this.state.project.location;
+          location.formatted_address = addressWithoutPostalCode;
+
+          this.setState({
+            isLocationLoaded: true,
+            projectLocation: location
+          });
+        },
+        error => {
+          isLocationLoaded: true, error;
+        }
+      );
+  }
+
+  render() {
+    const { project, projectLocation, error, isProjectLoaded } = this.state;
+
+    const videos = project.gallery_videos || [];
+    const images = project.gallery_images || [];
+
+    if (!videos.length && !images.length) {
+      images.push(project.project_image);
+    }
+
+    const eventDate = moment(project.event_date);
+
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isProjectLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div className="project">
+          <ProjectHeader project={project} />
+          <div className="row">
+            <div className="col-8">
+              <ProjectGallery images={images} videos={videos} />
+              <ProjectFooter
+                eventDate={eventDate}
+                projectAddress={projectLocation}
+              />
+            </div>
+            <div className="col-4">
+              <ProjectSideInfo project={project} />
+            </div>
+          </div>
+          <div className="project-details row">
+            <div className="col-8">
+              <ProjectDetails
+                project={project}
+                currentTab={this.props.match.params.tab}
+              />
+            </div>
+          </div>
+        </div>
+      );
+    }
+  }
+}
+
+function ProjectHeader(props) {
+  const project = props.project;
+
+  return (
+    <div className="header row mb-4 d-flex flex-column">
+      <h2 className="p-1 flex-fill">{project.name}</h2>
+      <div className="text-muted p-1 flex-fill">
+        {project.short_description}
+      </div>
+    </div>
+  );
+}
+
+function ProjectFooter(props) {
+  return (
+    <div className="d-flex">
+      <ProjectDate date={props.eventDate} />
+      {props.projectAddress ? (
+        <ProjectAddress address={props.projectAddress} />
+      ) : null}
+    </div>
+  );
+}
+
+function ProjectDate(props) {
+  const formattedDate = props.date.format("dddd, MMMM Do, hh:mm");
+  const isoDate = props.date.toISOString();
+
+  return (
+    <div className="mr-auto">
+      <Link to={"/date/" + isoDate} className="event-date">
+        <FontAwesomeIcon className="date-icon" icon={faCalendarAlt} />{" "}
+        {formattedDate}
+      </Link>
+    </div>
+  );
+}
+
+function ProjectAddress(props) {
+  const formattedAddress = props.address.formatted_address;
+  const latitude = props.address.latitude;
+  const longitude = props.address.longitude;
+
+  return (
+    <div className="ml-auto">
+      <Link to={"/place/" + latitude + "," + longitude} className="location">
+        <FontAwesomeIcon className="location-icon" icon={faMapMarkerAlt} />{" "}
+        {formattedAddress}
+      </Link>
+    </div>
+  );
+}
+
+function ProjectSideInfo(props) {
+  const project = props.project;
+
+  const raised = project.raised || { amount: 0 };
+
+  const categories = project.categories || [];
+
+  const fundingGoal = project.funding_goal || { amount: 1 };
+  const progress = Math.floor(raised.amount / fundingGoal.amount * 100);
+  const timeLeft = moment
+    .duration(moment(project.ends).diff(new moment()))
+    .format();
+
+  const owner = project.owner;
+  const ownerImage = owner.image_link || avatarPlaceholder;
+
+  return (
+    <div className="project-info px-2 d-flex flex-column flex-fill text-left">
+      <div className="project-owner info-block d-flex">
+      <Link className="image-link" to={"person/" + owner.id}>
+            <img className="image" src={ownerImage} />
+          </Link>
+          <div className="d-flex flex-column pl-3">
+            <Link className="owner-link" to={"person/" + owner.id}>
+              {owner.name} {owner.surname ? owner.surname : ""}
+            </Link>
+            <div className="text-muted">Project owner</div>
+          </div>
+      </div>
+      <div className="info-block progress">
+        <div
+          className="progress-bar"
+          style={{ width: progress + "%" }}
+          role="progressbar"
+          aria-valuenow={raised.amount}
+          aria-valuemin="0"
+          aria-valuemax={fundingGoal.amount}
+        />
+      </div>
+      <div className="info-block">
+        <span className="primary text-primary">
+          {raised.amount + "" + getSymbolFromCurrency(raised.currency)}{" "}
+        </span>
+        <span className="">{raised.currency} raised</span>
+        <div className="text-muted">
+          {progress}% of{" "}
+          {fundingGoal.amount +
+            "" +
+            getSymbolFromCurrency(fundingGoal.currency)}
+        </div>
+      </div>
+      <div className="info-block">
+        <span className="primary">{project.contributions.length}</span>
+        <div className="text-muted">contributors</div>
+      </div>
+      <div className="info-block">
+        <span className="primary">{timeLeft}</span>
+        <div className="text-muted">left</div>
+      </div>
+      <div className="info-block contribute">
+        <Link
+          to={"/contribute/" + project.id}
+          className="btn btn-primary w-100"
+        >
+          Contribute to the project
+        </Link>
+      </div>
+      <div className="info-block d-flex">
+        {categories.map(category => (
+          <Link
+            to={"/category/" + category.id}
+            className="category mr-3"
+            key={category.id}
+          >
+            <FontAwesomeIcon className="category-icon" icon={faTag} />{" "}
+            {category.name}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Project;
--- src/components/ProjectDetails.js
+++ src/components/ProjectDetails.js
@@ -2,11 +2,6 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import moment from "moment";
-import getSymbolFromCurrency from "currency-symbol-map";
-import Geocode from "react-geocode";
-
-import ProjectGallery from "./ProjectGallery";
-import ProjectDescription from "./ProjectDescription";
 
 import FontAwesomeIcon from "@fortawesome/react-fontawesome";
 import faHeart from "@fortawesome/fontawesome-free-regular/faHeart";
@@ -15,257 +10,405 @@ import faEnvelope from "@fortawesome/fontawesome-free-solid/faEnvelope";
 import faTag from "@fortawesome/fontawesome-free-solid/faTag";
 import faMapMarkerAlt from "@fortawesome/fontawesome-free-solid/faMapMarkerAlt";
 import faCalendarAlt from "@fortawesome/fontawesome-free-regular/faCalendarAlt";
+import faCaretRight from "@fortawesome/fontawesome-free-solid/faCaretRight";
+import faCaretDown from "@fortawesome/fontawesome-free-solid/faCaretDown";
 
 import "../css/ProjectDetails.css";
 
 import avatarPlaceholder from "../image/avatar_placeholder.png";
 
-class ProjectDetails extends Component {
+function ProjectDetails(props) {
+  let currentTab = "description";
+  if (props.currentTab === "faq") {
+    currentTab = "faq";
+  } else if (props.currentTab === "updates") {
+    currentTab = "updates";
+  } else if (props.currentTab === "comments") {
+    currentTab = "comments";
+  }
+
+  return (
+    <div className="project-details">
+      <ProjectNavigation active={currentTab} projectId={props.project.id} />
+      <ProjectTabs tab={currentTab} project={props.project} />
+    </div>
+  );
+}
+
+function ProjectNavigation(props) {
+  const baseClass = "nav-link";
+  const activeClass = " active";
+  const descriptionClass =
+    baseClass + (props.active === "description" ? activeClass : "");
+  const faqClass = baseClass + (props.active === "faq" ? activeClass : "");
+  const updatesClass =
+    baseClass + (props.active === "updates" ? activeClass : "");
+  const commentsClass =
+    baseClass + (props.active === "comments" ? activeClass : "");
+
+  return (
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <Link className={descriptionClass} to={"/project/" + props.projectId}>
+          Description
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link className={faqClass} to={"/project/" + props.projectId + "/faq"}>
+          FAQ
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          className={updatesClass}
+          to={"/project/" + props.projectId + "/updates"}
+        >
+          Updates
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          className={commentsClass}
+          to={"/project/" + props.projectId + "/comments"}
+        >
+          Comments
+        </Link>
+      </li>
+    </ul>
+  );
+}
+
+function ProjectTabs(props) {
+  if (props.tab === "description") {
+    return <DescriptionTab description={props.project.description} />;
+  } else if (props.tab === "comments") {
+    return <CommentsTab projectId={props.project.id} />;
+  } else if (props.tab === "faq") {
+    return <FaqsTab projectId={props.project.id} />;
+  } else if (props.tab === "updates") {
+    return <UpdatesTab projectId={props.project.id} />;
+  }
+}
+
+function DescriptionTab(props) {
+  return <p>{props.description}</p>;
+}
+
+class CommentsTab extends Component {
   constructor(props) {
     super(props);
     this.state = {
       error: null,
-      isProjectLoaded: false,
-      isLocationLoaded: false,
-      project: {}
+      isLoaded: false,
+      comments: []
     };
   }
 
   componentDidMount() {
     axios
-      .get("http://127.0.0.1:8080/v0/projects/" + this.props.id, {
-        crossdomain: true
-      })
+      .get(
+        "http://127.0.0.1:8080/v0/projects/" +
+          this.props.projectId +
+          "/comments",
+        {
+          crossdomain: true
+        }
+      )
       .then(
         result => {
           this.setState({
-            isProjectLoaded: true,
-            project: result.data
+            isLoaded: true,
+            comments: result.data.content
           });
         },
         error => {
           this.setState({
-            isProjectLoaded: true,
+            isLoaded: true,
             error
           });
-        }
-      )
-      .then(() =>
-        Geocode.fromLatLng(
-          this.state.project.location.latitude,
-          this.state.project.location.longitude
-        )
-      )
-      .then(
-        response => {
-          console.log(response);
-          const postalCode = response.results[0].address_components.filter(
-            component => component.types.includes("postal_code")
-          );
-          let addressWithoutPostalCode = response.results[0].formatted_address;
-          if (postalCode.length > 0) {
-            addressWithoutPostalCode = addressWithoutPostalCode.replace(
-              " " + postalCode[0].long_name,
-              ""
-            );
-          }
-
-          const location = this.state.project.location;
-          location.formatted_address = addressWithoutPostalCode;
-
-          this.setState({
-            isLocationLoaded: true,
-            projectLocation: location
-          });
-        },
-        error => {
-          isLocationLoaded: true, error;
         }
       );
   }
 
   render() {
-    const { project, projectLocation, error, isProjectLoaded } = this.state;
-
-    const videos = project.gallery_videos || [];
-    const images = project.gallery_images || [];
-
-    if (!videos.length && !images.length) {
-      images.push(project.project_image);
-    }
-
-    const eventDate = moment(project.event_date);
+    const { comments, error, isLoaded } = this.state;
 
     if (error) {
       return <div>Error: {error.message}</div>;
-    } else if (!isProjectLoaded) {
+    } else if (!isLoaded) {
       return <div>Loading...</div>;
     } else {
       return (
-        <div className="project-details">
-          <ProjectHeader project={project} />
-          <div className="row">
-            <div className="col-8">
-              <ProjectGallery images={images} videos={videos} />
-              <ProjectFooter
-                eventDate={eventDate}
-                projectAddress={projectLocation}
-              />
-            </div>
-            <div className="col-4">
-              <ProjectSideInfo project={project} />
-            </div>
-          </div>
-          <div className="project-description row">
-            <div className="col-8">
-              <ProjectDescription
-                project={project}
-                currentTab={this.props.currentTab}
-              />
-            </div>
-          </div>
+        <div>
+          {comments.map(comment => (
+            <Comment comment={comment} key={comment.id} />
+          ))}
         </div>
       );
     }
   }
 }
 
-function ProjectHeader(props) {
-  const project = props.project;
+function Comment(props) {
+  const comment = props.comment;
+  const time = moment(comment.posted).fromNow();
+  const image = comment.author.image_link || avatarPlaceholder;
 
   return (
-    <div className="header row mb-4 d-flex flex-column">
-      <h2 className="p-1 flex-fill">{project.name}</h2>
-      <div className="text-muted p-1 flex-fill">
-        {project.short_description}
-      </div>
-    </div>
-  );
-}
-
-function ProjectFooter(props) {
-  return (
-    <div className="d-flex">
-      <ProjectDate date={props.eventDate} />
-      {props.projectAddress ? (
-        <ProjectAddress address={props.projectAddress} />
-      ) : null}
-    </div>
-  );
-}
-
-function ProjectDate(props) {
-  const formattedDate = props.date.format("dddd, MMMM Do, hh:mm");
-  const isoDate = props.date.toISOString();
-
-  return (
-    <div className="mr-auto">
-      <Link to={"/date/" + isoDate} className="event-date">
-        <FontAwesomeIcon className="date-icon" icon={faCalendarAlt} />{" "}
-        {formattedDate}
-      </Link>
-    </div>
-  );
-}
-
-function ProjectAddress(props) {
-  const formattedAddress = props.address.formatted_address;
-  const latitude = props.address.latitude;
-  const longitude = props.address.longitude;
-
-  return (
-    <div className="ml-auto">
-      <Link to={"/place/" + latitude + "," + longitude} className="location">
-        <FontAwesomeIcon className="location-icon" icon={faMapMarkerAlt} />{" "}
-        {formattedAddress}
-      </Link>
-    </div>
-  );
-}
-
-function ProjectSideInfo(props) {
-  const project = props.project;
-
-  const raised = project.raised || { amount: 0 };
-
-  const categories = project.categories || [];
-
-  const fundingGoal = project.funding_goal || { amount: 1 };
-  const progress = Math.floor(raised.amount / fundingGoal.amount * 100);
-  const timeLeft = moment
-    .duration(moment(project.ends).diff(new moment()))
-    .format();
-
-  const owner = project.owner;
-  const ownerImage = owner.image_link || avatarPlaceholder;
-
-  return (
-    <div className="project-info px-2 d-flex flex-column flex-fill text-left">
-      <div className="project-owner info-block">
-        <div className="d-flex">
-          <Link className="image-link" to={"person/" + owner.id}>
-            <img className="image" src={ownerImage} />
-          </Link>
-          <div className="d-flex flex-column pl-3">
-            <Link className="owner-link" to={"person/" + owner.id}>
-              {owner.name} {owner.surname ? owner.surname : ""}
-            </Link>
-            <div className="text-muted">Project owner</div>
-          </div>
-        </div>
-      </div>
-      <div className="info-block progress">
-        <div
-          className="progress-bar"
-          style={{ width: progress + "%" }}
-          role="progressbar"
-          aria-valuenow={raised.amount}
-          aria-valuemin="0"
-          aria-valuemax={fundingGoal.amount}
-        />
-      </div>
-      <div className="info-block">
-        <span className="primary text-primary">
-          {raised.amount + "" + getSymbolFromCurrency(raised.currency)}{" "}
-        </span>
-        <span className="">{raised.currency} raised</span>
-        <div className="text-muted">
-          {progress}% of{" "}
-          {fundingGoal.amount +
-            "" +
-            getSymbolFromCurrency(fundingGoal.currency)}
-        </div>
-      </div>
-      <div className="info-block">
-        <span className="primary">{project.contributions.length}</span>
-        <div className="text-muted">contributors</div>
-      </div>
-      <div className="info-block">
-        <span className="primary">{timeLeft}</span>
-        <div className="text-muted">left</div>
-      </div>
-      <div className="info-block contribute">
-        <Link
-          to={"/contribute/" + project.id}
-          className="btn btn-primary w-100"
-        >
-          Contribute to the project
+    <div id={comment.id} className="comment row p-3">
+      <div className="col-2 text-center">
+        <Link className="image-link" to={"/person/" + comment.author.id}>
+          <img className="image" src={image} />
         </Link>
       </div>
-      <div className="info-block d-flex">
-        {categories.map(category => (
-          <Link
-            to={"/category/" + category.id}
-            className="category mr-3"
-            key={category.id}
-          >
-            <FontAwesomeIcon className="category-icon" icon={faTag} />{" "}
-            {category.name}
+      <div className="col-10 d-flex flex-column">
+        <div className="d-flex">
+          <Link className="author-link" to={"/person/" + comment.author.id}>
+            {comment.author.name}{" "}
+            {comment.author.surname ? comment.author.surname : ""}
           </Link>
-        ))}
+          <span className="text-muted pl-2">
+            <a className="comment-link" href={"#" + comment.id}>
+              {time}
+            </a>
+          </span>
+        </div>
+
+        <p>{comment.message}</p>
       </div>
     </div>
   );
+}
+
+class FaqsTab extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      faqs: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(
+        "http://127.0.0.1:8080/v0/projects/" + this.props.projectId + "/faqs",
+        {
+          crossdomain: true
+        }
+      )
+      .then(
+        result => {
+          this.setState({
+            isLoaded: true,
+            faqs: result.data.content
+          });
+        },
+        error => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      );
+  }
+
+  render() {
+    const { faqs, error, isLoaded } = this.state;
+
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div className="faqs" id="faqs">
+          {faqs.map(faq => <Faq faq={faq} key={faq.id} />)}
+        </div>
+      );
+    }
+  }
+}
+
+class Faq extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { toggled: false };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(prevState => ({
+      toggled: !prevState.toggled
+    }));
+  }
+
+  render() {
+    const faq = this.props.faq;
+    const { toggled } = this.state;
+
+    return (
+      <div className="faq card">
+        <div className="faq-header card-header fa" id={faq.id}>
+          <h5 className="mb-0">
+            <button
+              className="btn-toggle btn btn-link collapsed w-100 d-flex"
+              onClick={this.handleClick}
+              type="button"
+              data-toggle="collapse"
+              data-target={"#answer-" + faq.id}
+              aria-expanded={toggled}
+              aria-controls={"answer-" + faq.id}
+            >
+              <span>{faq.question}</span>
+              <FontAwesomeIcon
+                className="ml-auto"
+                icon={toggled ? faCaretDown : faCaretRight}
+              />
+            </button>
+          </h5>
+        </div>
+        <div
+          id={"answer-" + faq.id}
+          className="collapse"
+          aria-labelledby={faq.id}
+        >
+          <div className="answer card-body">{faq.answer}</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+class UpdatesTab extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      updates: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(
+        "http://127.0.0.1:8080/v0/projects/" +
+          this.props.projectId +
+          "/updates",
+        {
+          crossdomain: true
+        }
+      )
+      .then(
+        result => {
+          this.setState({
+            isLoaded: true,
+            updates: result.data.content
+          });
+        },
+        error => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      );
+  }
+
+  render() {
+    const { updates, error, isLoaded } = this.state;
+    console.log(updates);
+
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div className="updates">
+          {updates.map(update => <Update update={update} key={update.id} />)}
+        </div>
+      );
+    }
+  }
+}
+
+class Update extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      readMoreExpanded: false,
+      update: {}
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(prevState => ({
+      readMoreExpanded: !prevState.readMoreExpanded
+    }));
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://127.0.0.1:8080/v0/updates/" + this.props.update.id, {
+        crossdomain: true
+      })
+      .then(
+        result => {
+          this.setState({
+            isLoaded: true,
+            update: result.data
+          });
+        },
+        error => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      );
+  }
+
+  render() {
+    const { readMoreExpanded, isLoaded: isMessageLoaded, error } = this.state;
+
+    const update = this.props.update;
+    const formattedTime = moment(update.posted).format("MMMM Do, hh:mm");
+    const fromNow = moment(update.posted).fromNow();
+
+    const readMoreLessText = readMoreExpanded ? "Read less" : "Read more";
+
+    return (
+      <div className="update card mb-3">
+        <div className="card-body">
+          <h5 className="update-header card-title">
+            {update.title}
+          </h5>
+          <p className="short-message card-text">
+            {update.short_message}
+          </p>
+          <div className="collapse" id={"update-msg-" + update.id}>
+            <p className="full-message card-text">
+              {isMessageLoaded ? this.state.update.message || error : "Loading..."}
+            </p>
+          </div>
+          <a className="btn btn-link" onClick={this.handleClick} data-toggle="collapse" href={"#update-msg-" + update.id} role="button" aria-expanded="false" aria-controls={"update-msg-" + update.id}>
+            {readMoreLessText}
+          </a>
+          <p className="card-text d-flex">
+            <small className="text-muted">{formattedTime}</small>
+            <small className="text-muted ml-auto">{fromNow}</small>
+          </p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default ProjectDetails;
