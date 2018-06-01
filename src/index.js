4e7b10c2f613c003a369257ccd94ca034c817dc4
Seregy <seregy77@gmail.com>
2018-06-01 22:24:14 +0300

    Add initial configuration for internationalizing app with react-intl

--- src/index.js
+++ src/index.js
@@ -5,10 +5,27 @@ import "./css/index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
+import { IntlProvider, addLocaleData } from 'react-intl';
+import en from 'react-intl/locale-data/en';
+import uk from 'react-intl/locale-data/uk';
+
+import messages_uk from "./messages/uk.json";
+import messages_en from "./messages/en.json";
+
+const messages = {
+  "uk": messages_uk,
+  "en": messages_en
+}
+const locale = "uk";
+
+addLocaleData([...en, ...uk]);
+
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <IntlProvider locale={locale} messages={messages[locale]}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </IntlProvider>,
   document.getElementById("root")
 );
 registerServiceWorker();
