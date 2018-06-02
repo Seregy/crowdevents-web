7a22dd4a51a185cf8788e078ab78375a579b5908
Seregy <seregy77@gmail.com>
2018-06-03 00:10:22 +0300

    Internationalize ProjectRewards

--- src/components/ProjectRewards.js
+++ src/components/ProjectRewards.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import getSymbolFromCurrency from "currency-symbol-map";
+import { FormattedMessage, FormattedDate, FormattedNumber } from 'react-intl';
 
 import "../css/ProjectRewards.css";
 
@@ -19,15 +20,39 @@ function ProjectRewards(props) {
 
 function RewardCard(props) {
   const reward = props.reward;
-  const title = `Внесіть ${reward.minimal_contribution.amount + getSymbolFromCurrency(reward.minimal_contribution.currency)} або більше`;
-
-  const left = reward.limit != null ? (`Залишилось ${reward.limit - reward.contributions.length}`) : "Необмежена кількість";
+  let left;
+  if (reward.limit) {
+    left = (
+      <FormattedMessage
+        id="app.project.reward.amount.left"
+        defaultMessage="{amount} left"
+        values={{
+          amount: reward.limit - reward.contributions.length,
+        }}
+      />
+    );
+  } else {
+    left = (
+      <FormattedMessage
+        id="app.project.reward.amount.unlimited"
+        defaultMessage="Unlimited amount"
+      />
+    );
+  }
 
   let shipping;
   if (reward.shipping_location) {
     shipping = (
       <div>
-        <h6 className="card-subtitle mb-2 text-muted">Доставка: {reward.shipping_location}</h6>
+        <h6 className="card-subtitle mb-2 text-muted">
+          <FormattedMessage
+            id="app.project.reward.delivery.location"
+            defaultMessage="Delivery location: {location}"
+            values={{
+              location: reward.shipping_location,
+            }}
+          />
+        </h6>
       </div>
     );
   }
@@ -36,7 +61,15 @@ function RewardCard(props) {
   if (reward.delivery_date) {
     delivery = (
       <div>
-        <h6 className="card-subtitle mb-2 text-muted">Дата доставки: {reward.delivery_date}</h6>
+        <h6 className="card-subtitle mb-2 text-muted">
+          <FormattedMessage
+            id="app.project.reward.delivery.date"
+            defaultMessage="Delivery date: {date}"
+            values={{
+              date: reward.delivery_date,
+            }}
+          />
+        </h6>
       </div>
     );
   }
@@ -44,12 +77,34 @@ function RewardCard(props) {
   return (
     <div className="reward card">
       <div className="card-body">
-        <h5 className="card-title">{title}</h5>
+        <h5 className="card-title">
+          <FormattedMessage
+            id="app.project.reward.minimalContribution.text"
+            defaultMessage="Contribute {amountWithCurrency} or more"
+            values={{
+              amountWithCurrency: (
+                <FormattedMessage
+                  id="app.project.reward.minimalContribution.amountWithSymbol"
+                  defaultMessage="{currencySymbol}{amount}"
+                  values={{
+                    amount: reward.minimal_contribution.amount,
+                    currencySymbol: getSymbolFromCurrency(reward.minimal_contribution.currency)
+                  }}
+                />
+              )
+            }}
+          />
+        </h5>
         <h6 className="card-subtitle mb-2 text-muted">{left}</h6>
         <p className="card-text">{reward.description}</p>
         {shipping}
         {delivery}
-        <Link to="#" className="btn btn-primary w-100">Зробити внесок</Link>
+        <Link to="#" className="btn btn-primary w-100">
+          <FormattedMessage
+            id="app.project.contribute"
+            defaultMessage="Contribute"
+          />
+        </Link>
       </div>
     </div>
   );
